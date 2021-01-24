21e96d090a9c224d555f87fba316f43289a37f7e
Trần Trung Nhật <tneils@MacBook-Pro-cua-Tran.local>
2021-01-24 23:42:54 +0700

    update labwork 12

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,24 +1,67 @@
 package vn.edu.usth.usthweather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabLayout;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
     private final static String TAG = "WeatherActivity";
-//    private AppBarConfiguration appBarConfiguration;
-
+    //    private AppBarConfiguration appBarConfiguration;
+    private MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +77,10 @@ public class WeatherActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
-        MediaPlayer player = MediaPlayer.create(this, R.raw.song);
+
+        player = MediaPlayer.create(this, R.raw.song);
         player.start();
     }
-
-
-    
 
     @Override
     protected void onStart() {
@@ -62,6 +103,9 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+        player.stop();
+        player.release();
+        player = null;
         Log.i(TAG, "on stop...");
     }
 
@@ -73,34 +117,50 @@ public class WeatherActivity extends AppCompatActivity {
 
     public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
         private final int PAGE_COUNT = 3;
-        private String titles[] = new String[]{"Hanoi", "Paris", "Toulouse"};
-
+        private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
         public HomeFragmentPagerAdapter(FragmentManager fm) {
             super(fm);
         }
-
         @Override
         public int getCount() {
             return PAGE_COUNT; // number of pages for a ViewPager
         }
-
         @Override
         public Fragment getItem(int page) {
 // returns an instance of Fragment corresponding to the specified page
             switch (page) {
-                case 0:
-                    return new WeatherAndForecastFragment();
-                case 1:
-                    return new WeatherAndForecastFragment();
-                case 2:
-                    return new WeatherAndForecastFragment();
+                case 0: return new WeatherAndForecastFragment();
+                case 1: return new WeatherAndForecastFragment();
+                case 2: return new WeatherAndForecastFragment();
             }
             return new Fragment();
         }
-
         @Override
         public CharSequence getPageTitle(int page) {
+// returns a tab title corresponding to the specified page
             return titles[page];
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refreshing...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Log.d(TAG, "onOptionsItemSelected: click");
+                Intent intent = new Intent(this, PrefActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
